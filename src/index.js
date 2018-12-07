0b7ab329fc5f67fe7bddbfd5d785f6273b958a45
Igor Laborie <ilaborie@gmail.com>
2018-12-07 16:27:25 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-import {render} from 'lit-html';
+import {html, render} from 'lit-html';
 import {repeat} from 'lit-html/directives/repeat';
+import {data} from './data'
 
 const body = document.querySelector("body");
 
-
-const state = {
-    talks: null,
-    current: null
-};
-
+const talkListView = talk => html`
+<div class="list-talk">
+  <span class="startTime">${talk.startTime}</span>    
+  <span class="endTime">${talk.endTime}</span>    
+  <span class="title">${talk.title}</span>    
+  <span class="speakers">${talk.speakers.map(it => `${it.firstName} ${it.lastName}`)}</span>    
+</div>`;
 
 const listView = state => html`
 <main class="${state.list ? 'list ' : ''}${state.current ? 'current ' : ''}">
   
-${repeat(state.list || [], (talk) => talk.id, (talk) => html`<div class="talk">${JSON.stringify(talk)}</div>`}
+${repeat(state.talks || [], (talk) => talk.id, talkListView)}
+
+${state.current ? `<div class="detail">${JSON.stringify(state.current)}</div>` : ''}  
   
-  <div class="detail">${JSON.stringify(state.current)}</div>
-</main>
-    
-`;
+</main>`;
 
-const display = state =>
+const display = state => {
+    console.dir(state);
     render(listView(state), body);
+};
 
-fetch('https://monkeyconf.herokuapp.com/')
-    .then(res => res.json())
-    .then(display);
+// fetch('https://monkeyconf.herokuapp.com/', {mode: "no-cors"})
+// fetch('/data.json')
+//     .then(res => res.json())
+//     .then(talks => display({talks, current: null}));
+
+display({talks: data, current: null});
